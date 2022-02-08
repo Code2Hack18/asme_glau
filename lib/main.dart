5c2f2b438ed6afdc1829e67ab48ad341e22a004a
Utkarsh ojha <utkicon2021@gmail.com>
2022-02-08 20:51:22 +0530

    2 commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,37 @@ class MyApp extends StatelessWidget {
           title: const Text('Home'),
         ),
         body: Container(),
-        drawer: const Drawer(),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: const <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blueGrey,
+                ),
+                child: Text(
+                  'User',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.message),
+                title: Text('Messages'),
+              ),
+              ListTile(
+                leading: Icon(Icons.account_circle),
+                title: Text('Profile'),
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text('Settings'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
